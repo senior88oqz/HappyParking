f3ad508e8d1148d6202c94a68a454df473bcd620
yukexie <38680961+yukexie@users.noreply.github.com>
2018-09-09 10:28:48 +1000

    area searching

--- server/area_searching.py
+++ server/area_searching.py
@@ -1,15 +1,16 @@
 import geopandas as gpd
+import json
 from shapely.geometry import Point
 from shapely.geometry import LinearRing
 from math import radians, cos, sin, asin, sqrt
-from bay_sensor import data2geojson
+from bay_sensor import data2geojson, getRealTimeData
 
 # @params: lon: longitude
 # @params: lat: latitude
 # @params: parks: geojson
 
 def availableParks(inputGeojson):
-    circle = Point(144.969696,-37.809298)
+    circle = Point(144.968492,-37.797397)
     parks_data = gpd.read_file(inputGeojson)
     avi_parks = []
     for geom in parks_data['geometry']:
@@ -24,10 +25,12 @@ def availableParks(inputGeojson):
             dis = haversine(circle.x,circle.y,p[0],p[1])
         if (dis < 500):
             avi_parks.append(geom)
-    outputgeojson = parks_data.loc[parks_data['geometry'].isin(avi_parks)]
-    outputgeojson = outputgeojson.loc[outputgeojson['occupied'] == false]
-    outputgeojson = data2geojson(outputgeojson)
-    return outputgeojson
+    if(len(avi_parks)!= 0):
+        outputgeojson = parks_data.loc[parks_data['geometry'].isin(avi_parks)]
+        outputgeojson = outputgeojson.loc[outputgeojson['status'] == 'Unoccupied']
+        outputgeojson = data2geojson(outputgeojson)
+        return outputgeojson
+    else: return 0
 
 def findNearestPoint(geom,circle):
     pol_ext = LinearRing(geom.exterior.coords)
@@ -51,37 +54,8 @@ def haversine(lon1, lat1, lon2, lat2):
     # Radius of earth in kilometers is 6371
     m = 6371 * c * 1000
     return m
-#
-# def data2geojson(gdf):
-#     geoJson = {'type': 'FeatureCollection', 'features': []}
-#     with open('park_config.json') as json_doc:
-#         style = json.load(json_doc)
-#     for i in range(len(gdf)):
-#         visual = style[gdf['status'][i]]
-#         res = {'p_tag': gdf['restriction']['p_tag'][i],
-#                'duration': gdf['restriction']['duration'][i],
-#                'is_charged': gdf['restriction']['is_charged'][i],
-#                'timestart': gdf['restriction']['timestart'][i],
-#                'timeend': gdf['restriction']['timeend'][i],
-#         }
-#         temp = {"bay_id": gdf['bay_id'][i],
-#                 "occupied": gdf["occupied"][i],
-#                 "only_for_disabled": gdf['only_for_disabled'][i],
-#                 'restriction':res
-#         }
-#         properties = dict(temp, **visual)
-#         feature = {'type': 'Feature',
-#                'properties': properties,
-#                'geometry': gdf['geometry'][i]
-#                }
-#         geoJson['features'].append(feature)
-#     with open('bay_sensor.geojson','w', encoding='utf-8') as f:
-#         output = json.dump(geoJson, f)
-#         return output
-
-    #def availableRestriction(gdf, P):
-    # for restriction matching
 
     # test
-    # aParks = availableParks(144.971755,-37.802740,'On-street Parking Bays.geojson')
+    #data = getRealTimeData()
+    #aParks = availableParks('On-street Parking Bays.geojson')
     # data2geojson(aParks)
